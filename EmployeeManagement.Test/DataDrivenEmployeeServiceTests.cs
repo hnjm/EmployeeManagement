fcb75ef2501ea6acd5ac0b7cf6ad514d1c729b03
Wagner Bolfe <shaper@live.com>
2023-03-21 14:56:55 -0300

    Added test with external file

--- EmployeeManagement.Test/DataDrivenEmployeeServiceTests.cs
+++ EmployeeManagement.Test/DataDrivenEmployeeServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using EmployeeManagement.DataAccess.Entities;
 using EmployeeManagement.Test.Fixtures;
+using EmployeeManagement.Test.TestData;
 using Xunit;
 
 namespace EmployeeManagement.Test
@@ -9,9 +10,27 @@ namespace EmployeeManagement.Test
     {
         private readonly EmployeeServiceFixture _employeeServiceFixture;
 
-        public DataDrivenEmployeeServiceTests(EmployeeServiceFixture employeeServiceFixture)
+        public DataDrivenEmployeeServiceTests(
+            EmployeeServiceFixture employeeServiceFixture)
         {
             _employeeServiceFixture = employeeServiceFixture;
+        }
+
+        [Theory]
+        [InlineData("1fd115cf-f44c-4982-86bc-a8fe2e4ff83e")]
+        [InlineData("37e03ca7-c730-4351-834c-b66f280cdb01")]
+        public void CreateInternalEmployee_InternalEmployeeCreated_MustHaveAttendedSecondObligatoryCourse(
+            Guid courseId)
+        {
+            // Arrange 
+
+            // Act
+            var internalEmployee = _employeeServiceFixture.EmployeeService
+                .CreateInternalEmployee("Brooklyn", "Cannon");
+
+            // Assert
+            Assert.Contains(internalEmployee.AttendedCourses,
+                course => course.Id == courseId);
         }
 
         [Fact]
@@ -44,5 +63,65 @@ namespace EmployeeManagement.Test
             // Assert
             Assert.False(internalEmployee.MinimumRaiseGiven);
         }
+
+        public static IEnumerable<object[]> ExampleTestDataForGiveRaiseWithProperty
+        {
+            get
+            {
+                return new List<object[]>
+                {
+                        new object[] { 100, true },
+                        new object[] { 200, false }
+                };
+            }
+        }
+
+        public static TheoryData<int, bool> StronglyTypedExampleTestDataForGiveRaiseWithProperty
+        {
+            get
+            {
+                return new TheoryData<int, bool>()
+                {
+                        { 100, true },
+                        { 200, false }
+                };
+            }
+        }
+
+        public static IEnumerable<object[]> ExampleTestDataForGiveRaise_WithMethod(
+             int testDataInstancesToProvide)
+        {
+            var testData = new List<object[]>
+                {
+                        new object[] { 100, true },
+                        new object[] { 200, false }
+                };
+
+            return testData.Take(testDataInstancesToProvide);
+        }
+
+
+        [Theory]
+        //[MemberData(
+        //    nameof(DataDrivenEmployeeServiceTests.ExampleTestDataForGiveRaiseWithMethod),
+        //    1,
+        //    MemberType = typeof(DataDrivenEmployeeServiceTests))]
+        //[ClassData(typeof(EmployeeServiceTestData))]
+        //[ClassData(typeof(StronglyTypedEmployeeServiceTestData))]
+        //[MemberData(nameof(StronglyTypedExampleTestDataForGiveRaiseWithProperty))]
+        [ClassData(typeof(StronglyTypedEmployeeServiceTestDataFromFile))]
+        public async Task GiveRaise_RaiseGiven_EmployeeMinimumRaiseGivenMatchesValue(
+            int raiseGiven, bool expectedValueForMinimumRaiseGiven)
+        {
+            // Arrange  
+            var internalEmployee = new InternalEmployee("Brooklyn", "Cannon", 5, 3000, false, 1);
+
+            // Act
+            await _employeeServiceFixture.EmployeeService.GiveRaiseAsync(internalEmployee, raiseGiven);
+
+            // Assert
+            Assert.Equal(expectedValueForMinimumRaiseGiven, internalEmployee.MinimumRaiseGiven);
+        }
+
     }
 }
---
+++ EmployeeManagement.Test/TestData/StronglyTypedEmployeeServiceTestDataFromFile.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace EmployeeManagement.Test.TestData
+{
+    public class StronglyTypedEmployeeServiceTestDataFromFile : TheoryData<int,bool>
+    {
+        public StronglyTypedEmployeeServiceTestDataFromFile()
+        {
+            var testDataLines = File.ReadAllLines("TestData/EmployeeServiceTestData.csv");
+         
+            foreach (var line in testDataLines)
+            {
+                // split the string
+                var splitString = line.Split(',');
+                // try parsing 
+                if (int.TryParse(splitString[0], out int raise) && bool.TryParse(splitString[1], out bool minimumRaiseGiven))
+                {
+                    // add test data
+                    Add(raise, minimumRaiseGiven);
+                }
+            }
+        }
+    }
+}
